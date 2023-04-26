767afc4dff841e1af83bed1efa4ebfced89b077a
arifremal <arifremal013@gmail.com>
2023-04-26 14:10:49 +0600

    first commit

---
+++ firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALFXKoYIwLqSILFEz00W3HCBPAz_shNpY",
+  authDomain: "the-news-dragon-5da00.firebaseapp.com",
+  projectId: "the-news-dragon-5da00",
+  storageBucket: "the-news-dragon-5da00.appspot.com",
+  messagingSenderId: "96688165136",
+  appId: "1:96688165136:web:4fb299b9245438f6fd5595",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
---
+++ src/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Shared/Header";
+import Footer from "./Shared/Footer";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Layout;
